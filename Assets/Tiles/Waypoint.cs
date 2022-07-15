d8b906f5773bb2f2664d3268c8566137d28299bb
katene30 <katenejack@gmail.com>
2022-07-16 00:47:27 +1200

    currency system, towers now cost and when you go bankrupt the scene reloads

--- Assets/Tiles/Waypoint.cs
+++ Assets/Tiles/Waypoint.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    [SerializeField] GameObject towerPrefab;
+    [SerializeField] Tower towerPrefab;
     
     [SerializeField] bool isPlaceable;
     public  bool IsPlaceable{get { return isPlaceable; } }
 
 
+
+
     void OnMouseDown() {
-        if(isPlaceable){
-            Instantiate(towerPrefab,transform.position, Quaternion.identity);
-            isPlaceable = false;
+        if(isPlaceable)
+        {
+            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
+            isPlaceable = !isPlaced;
         }
     }
 }
